d3706ec31f95c5ac497b120a142aa150b0580902
molarmanful <molarmanful@users.noreply.github.com>
2015-05-01 18:04:25 -0700

    Update timer.js

--- timer.js
+++ timer.js
@@ -218,6 +218,7 @@ $(window).on('orientationchange', function(){
 $('#subet').click(function(){
   if($('#et').val().replace(/\d/g, '') == ':.' && $('#et').val().split(/:|\./g)[1].length <= 2 && $('#et').val().split(/:|\./g)[1].length > 0 && $('#et').val().split(/:|\./g)[2].length <= 3){
     times[sn].push($('#et').val());
+    $('#et').val('');
     $('.input-group').removeClass('has-error');
     updatestats();
   } else {
